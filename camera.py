1cc1f602325765ab2241f967e345fdbd5ed418a3
joeylmaalouf <joeylmaalouf@gmail.com>
2015-06-02 11:31:48 -0400

    tweaks

--- camera.py
+++ camera.py
@@ -4,26 +4,26 @@ import numpy as np
 import cv2
 
 
-class Window(object):
+class VideoWindow(object):
 	def __init__(self, window_root, video_capture):
-		super(Window, self).__init__()
+		super(VideoWindow, self).__init__()
 		self.root = window_root
 		self.cap = video_capture
 		root.bind("<Escape>", lambda e: root.quit())
 		self.label = tk.Label(self.root, image = None)
 		self.label.pack()
-		self.button = tk.Button(self.root, text = "Send image to convnet", command = self.predict_class)
+		self.button = tk.Button(self.root, text = "Send image to convnet as 28x28 digit picture.", command = self.predict_class)
 		self.button.pack()
 		# cam_width, cam_height = int(cap.get(3)), int(cap.get(4))
 		# 640x480 on our school laptops
 
-	def updateFrame(self):
+	def getFrame(self):
 		ret, frame = self.cap.read()
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		return frame
 
-		b,g,r = cv2.split(frame)
-		frame = cv2.merge((r,g,b))
-
-		imgtk = ImageTk.PhotoImage(image = Image.fromarray(frame))
+	def updateFrame(self):
+		imgtk = ImageTk.PhotoImage(image = Image.fromarray(self.getFrame()))
 		self.label.configure(image = imgtk)
 		self.label.image = imgtk
 		self.root.after(100, self.updateFrame)
@@ -34,11 +34,11 @@ class Window(object):
 		cap.release()
 
 	def predict_class(self):
+		frame = cv2.resize(self.getFrame(), (28, 28))
 		# connect to CNN here
-		pass
 
 if __name__ == "__main__":
 	root = tk.Tk()
 	cap = cv2.VideoCapture(0)
-	win = Window(root, cap)
+	win = VideoWindow(root, cap)
 	win.run()
