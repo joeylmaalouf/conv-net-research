7274cca9d34b5315e1889cad5e0bfeb80ed54915
SeanCCarter <seancarter26@gmail.com>
2015-06-03 16:20:18 -0400

    Refactored code in Modern Net and related openCV program.

--- mnist_modern_net/OpenCV_Thingie.py
+++ mnist_modern_net/OpenCV_Thingie.py
@@ -8,7 +8,7 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 import numpy as np
 import cPickle
 
-from scipy.stats import mode
+import modern_net.py
 
 webcam_video = cv2.VideoCapture(0)
 srng = RandomStreams()
@@ -25,63 +25,6 @@ def scale_image(frame):
 	# gray = cv2.dilate(gray,kernal,iterations=1)
 	image = 255-image
 	return image
-def load_network(filename):
-	X = T.fmatrix()
-	Y = T.fmatrix()
-	f = open(filename, 'rb')
-	weights = cPickle.load(f)
-	w_h = weights[0]
-	w_h2 = weights[1]
-	w_o = weights[2]
-	f.close()
-	return w_h, w_h2, w_o
-
-
-def floatX(X):
-    return np.asarray(X, dtype=theano.config.floatX)
-
-def init_weights(shape):
-    return theano.shared(floatX(np.random.randn(*shape) * 0.01))
-
-def rectify(X):
-    return T.maximum(X, 0.)
-
-def softmax(X):
-    e_x = T.exp(X - X.max(axis=1).dimshuffle(0, 'x'))
-    return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
-
-def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
-    grads = T.grad(cost=cost, wrt=params)
-    updates = []
-    for p, g in zip(params, grads):
-        acc = theano.shared(p.get_value() * 0.)
-        acc_new = rho * acc + (1 - rho) * g ** 2
-        gradient_scaling = T.sqrt(acc_new + epsilon)
-        g = g / gradient_scaling
-        updates.append((acc, acc_new))
-        updates.append((p, p - lr * g))
-    return updates
-
-def dropout(X, p=0.):
-    if p > 0:
-        retain_prob = 1 - p
-        X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
-        X /= retain_prob
-    return X
-
-def model(X, w_h, w_h2, w_o, p_drop_input, p_drop_hidden):
-    X = dropout(X, p_drop_input)
-    h = rectify(T.dot(X, w_h))
-
-    h = dropout(h, p_drop_hidden)
-    h2 = rectify(T.dot(h, w_h2))
-
-    h2 = dropout(h2, p_drop_hidden)
-    py_x = softmax(T.dot(h2, w_o))
-    return h, h2, py_x
-
-def get_input(image):
-	size = image.shape()
 
 def mode(group):
 	d = {}
@@ -105,16 +48,9 @@ def image_accumulator(tracker, guess):
 
 
 if __name__ == '__main__':
-	X = T.fmatrix()
-	Y = T.fmatrix()
-
-	w_h, w_h2, w_o = load_network("MNIST_Network_CPU.save")
-
-	noise_h, noise_h2, noise_py_x = model(X, w_h, w_h2, w_o, 0.2, 0.5)
-	h, h2, py_x = model(X, w_h, w_h2, w_o, 0., 0.)
-	y_x = T.argmax(py_x, axis=1)
-
-	predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
+	mnn = modern_net.ModernNeuralNetwork()
+	mnn.load_weights("MNIST_Network_CPU.save")
+	mnn.create_model_functions()
 
 	tracker = [0 for i in range(180)]
 
@@ -124,7 +60,7 @@ if __name__ == '__main__':
 		image = scale_image(frame)
 		# Display the resulting frame
 		
-		tracker, guess = image_accumulator(tracker, int(predict(image.reshape(1, 784))))
+		tracker, guess = image_accumulator(tracker, int(mnn.predict(image.reshape(1, 784))))
 		print guess
 
 		cv2.imshow('frame', cv2.resize(image, (640,640)))
--- mnist_modern_net/modern_net.py
+++ mnist_modern_net/modern_net.py
@@ -7,87 +7,108 @@ import cPickle
 
 srng = RandomStreams()
 
-def floatX(X):
-    return np.asarray(X, dtype=theano.config.floatX)
+class ModernNeuralNetwork(object):
+    def __init__(self):
+        theano.config.floatX = "float32"
+        self.srng = RandomStreams()
+        self.X = T.fmatrix()
+        self.Y = T.fmatrix()
 
-def init_weights(shape):
-    return theano.shared(floatX(np.random.randn(*shape) * 0.01))
+    def floatX(self, X):
+        return np.asarray(X, dtype=theano.config.floatX)
 
-def rectify(X):
-    return T.maximum(X, 0.)
+    def init_weights(self, shape):
+        return theano.shared(self.floatX(np.random.randn(*shape) * 0.01))
 
-def softmax(X):
-    e_x = T.exp(X - X.max(axis=1).dimshuffle(0, 'x'))
-    return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
+    def rectify(self, X):
+        return T.maximum(X, 0.)
 
-def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
-    grads = T.grad(cost=cost, wrt=params)
-    updates = []
-    for p, g in zip(params, grads):
-        acc = theano.shared(p.get_value() * 0.)
-        acc_new = rho * acc + (1 - rho) * g ** 2
-        gradient_scaling = T.sqrt(acc_new + epsilon)
-        g = g / gradient_scaling
-        updates.append((acc, acc_new))
-        updates.append((p, p - lr * g))
-    return updates
+    def softmax(self, X):
+        e_x = T.exp(X - X.max(axis=1).dimshuffle(0, 'x'))
+        return e_x / e_x.sum(axis=1).dimshuffle(0, 'x')
 
-def dropout(X, p=0.):
-    if p > 0:
-        retain_prob = 1 - p
-        X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
-        X /= retain_prob
-    return X
+    def RMSprop(self, cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
+        grads = T.grad(cost=cost, wrt=params)
+        updates = []
+        for p, g in zip(params, grads):
+            acc = theano.shared(p.get_value() * 0.)
+            acc_new = rho * acc + (1 - rho) * g ** 2
+            gradient_scaling = T.sqrt(acc_new + epsilon)
+            g = g / gradient_scaling
+            updates.append((acc, acc_new))
+            updates.append((p, p - lr * g))
+        return updates
 
-def model(X, w_h, w_h2, w_o, p_drop_input, p_drop_hidden):
-    X = dropout(X, p_drop_input)
-    h = rectify(T.dot(X, w_h))
+    def dropout(self, X, p=0.):
+        if p > 0:
+            retain_prob = 1 - p
+            X *= srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
+            X /= retain_prob
+        return X
 
-    h = dropout(h, p_drop_hidden)
-    h2 = rectify(T.dot(h, w_h2))
+    def model(self, X, w_h, w_h2, w_o, p_drop_input, p_drop_hidden):
+        X = self.dropout(X, p_drop_input)
+        h = self.rectify(T.dot(X, w_h))
 
-    h2 = dropout(h2, p_drop_hidden)
-    py_x = softmax(T.dot(h2, w_o))
-    return h, h2, py_x
+        h = self.dropout(h, p_drop_hidden)
+        h2 = self.rectify(T.dot(h, w_h2))
 
-def add_noise(matrix):
-    shape = matrix.shape
-    noise = floatX(np.random.random(shape) * 2.5)
-    other_noise = floatX(np.ones(shape)*25 * np.random.random())
-    matrix = matrix + noise + other_noise
-    result = np.minimum(matrix, 255)
-    return result
+        h2 = self.dropout(h2, p_drop_hidden)
+        py_x = self.softmax(T.dot(h2, w_o))
+        return h, h2, py_x
 
-trX, teX, trY, teY = mnist(onehot=True)
-trX = add_noise(trX)
-trY = add_noise(trY)
+    def initialize_mnist(self):
+        self.trX, self.teX, self.trY, self.teY = mnist(onehot=True)
+
+        self.w_h = self.init_weights((784, 625))
+        self.w_h2 = self.init_weights((625, 625))
+        self.w_o = self.init_weights((625, 10))
+
+    def create_model_functions(self):
+        self.noise_h, self.noise_h2, self.noise_py_x = self.model(self.X, self.w_h, self.w_h2, self.w_o, 0.2, 0.5)
+        self.h, self.h2, self.py_x = self.model(self.X, self.w_h, self.w_h2, self.w_o, 0., 0.)
+        self.y_x = T.argmax(self.py_x, axis=1)
+
+        self.cost = T.mean(T.nnet.categorical_crossentropy(self.noise_py_x, self.Y))
+        self.params = [self.w_h, self.w_h2, self.w_o]
+        self.updates = self.RMSprop(self.cost, self.params, lr=0.001)
+        self.train = theano.function(inputs=[self.X, self.Y], outputs=self.cost, updates=self.updates, allow_input_downcast=True)
+        self.predict = theano.function(inputs=[self.X], outputs=self.y_x, allow_input_downcast=True)
+
+    def save_weights(self, filename):
+        saved_model = [self.w_h,self.w_h2,self.w_o]
+        f = file(filename, 'wb')
+        cPickle.dump(saved_model, f, protocol=cPickle.HIGHEST_PROTOCOL)
+        f.close()
+
+    def load_weights(self, filename):
+        f = open(filename, 'wb')
+        weights = cPickle.load()
+        self.w_h = weights[0]
+        self.w_h2 = weights[1]
+        self.w_o = weights[2]
+        f.close()
+
+    def train_mnist(self, verbose, epochs = 10):
+        for i in range(10):
+            for start, end in zip(range(0, len(self.trX), 128), range(128, len(self.trX), 128)):
+                cost = self.train(self.trX[start:end], self.trY[start:end])
+            if verbose:
+                print np.mean(np.argmax(self.teY, axis=1) == self.predict(self.teX))
+
+    def mnist_example(self, verbose = False, save = False):
+        print "Initilizing network"
+        self.initialize_mnist()
+        print "Creating Model"
+        self.create_model_functions()
+        print "Training Network"
+        self.train_mnist(verbose, 50)
+        if save:
+            self.save_weights("MNIST_Weights.save")
+            print("Saved weights to \"MNIST_Weights.save\".")
 
 
-X = T.fmatrix()
-Y = T.fmatrix()
-
-w_h = init_weights((784, 625))
-w_h2 = init_weights((625, 625))
-w_o = init_weights((625, 10))
-
-noise_h, noise_h2, noise_py_x = model(X, w_h, w_h2, w_o, 0.2, 0.5)
-h, h2, py_x = model(X, w_h, w_h2, w_o, 0., 0.)
-y_x = T.argmax(py_x, axis=1)
-
-cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, Y))
-params = [w_h, w_h2, w_o]
-updates = RMSprop(cost, params, lr=0.001)
-
-train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
-predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
-
-for i in range(10):
-    for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
-        cost = train(trX[start:end], trY[start:end])
-    print np.mean(np.argmax(teY, axis=1) == predict(teX))
-
-print "Saving Network"
-saved_model = [w_h,w_h2,w_o]
-f = file('MNIST_Network_Noise.save', 'wb')
-cPickle.dump(saved_model, f, protocol=cPickle.HIGHEST_PROTOCOL)
-f.close()
+if __name__ == '__main__':
+    mnn = ModernNeuralNetwork()
+    mnn.mnist_example(verbose = True, save = True)
+    print("Program complete.")
