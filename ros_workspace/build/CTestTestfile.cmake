1aa586dcfe88b8cdb9b3725a449338ec9f40e22f
joeylmaalouf <joeylmaalouf@gmail.com>
2015-06-04 10:27:26 -0400

    cleaned up

--- ros_workspace/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joey/github/conv-net-ella/catkin_ws/src
-# Build directory: /home/joey/github/conv-net-ella/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
