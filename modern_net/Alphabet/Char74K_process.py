4ea2b21f2473d1d8d101ba32de859101bc7bd03c
SeanCCarter <seancarter26@gmail.com>
2015-06-05 11:17:59 -0700

    Finale bugfixes

--- modern_net/Alphabet/Char74K_process.py
+++ modern_net/Alphabet/Char74K_process.py
@@ -11,14 +11,14 @@ def scale_image(image):
 	size = max(image.shape)
 	image = np.pad(image, (size, size), 'constant', constant_values=(0,0))
 	image = cv2.resize(image, (100,100))
-	image = np.reshape((1,1000))
+	image = np.reshape(image, (1,10000))
 	return image
 
 def create_image_with_mask(image, mask):
-	return bitwise_and(image, mask)
+	return cv2.bitwise_and(image, mask)
 
 if __name__ == '__main__':
-	image_array = np.array
+	image_array = None
 	values = []
 	"~/Research/alphabet_Data/English/Img/GoodImg"
 	for i in range(1,63):
@@ -28,13 +28,18 @@ if __name__ == '__main__':
 			sample = 'Sample0' + str(i)
 		listdirimg = os.listdir("./alphabet_Data/English/Img/GoodImg/Bmp/" + sample)
 		listdirmask = os.listdir("./alphabet_Data/English/Img/GoodImg/Msk/" + sample)
-		for i in range(len(listdirimg)):
-			image = cv2.imread("./alphabet_Data/English/Img/GoodImg/Bmp/" + sample + "/" + listdirimg[i])
-			mask = cv2.imread("./alphabet_Data/English/Img/GoodImg/Msk/" + sample + "/" + listdirmask[i])
+		for j in range(len(listdirimg)):
+			image = cv2.imread("./alphabet_Data/English/Img/GoodImg/Bmp/" + sample + "/" + listdirimg[j])
+			mask = cv2.imread("./alphabet_Data/English/Img/GoodImg/Msk/" + sample + "/" + listdirmask[j])
 			image = create_image_with_mask(image, mask)
 			image = scale_image(image)
-			np.append(image_array,image, axis = 0)
+			if image_array != None:
+				np.append(image_array,image, axis = 0)
+			else:
+				image_array = np.array(image)
+			values.append(i)
 	f = open("Char74k_data.save", "wb")
-	cPickle.dump(f)
+	cPickle.dump(image_array,f)
+	cPickle.dump(values, f)
 	f.close()
 		
