1757b4af7413f8377ea797087cc5cde10ac33fec
joeylmaalouf <joeylmaalouf@gmail.com>
2015-06-05 09:42:38 -0400

    starting ELLA

--- ros_workspace/src/image/camera.py
+++ ros_workspace/src/image/camera.py
@@ -8,12 +8,14 @@ from cv_bridge import CvBridge, CvBridgeError
 
 class Decompresser(object):
 	def __init__(self):
-		self.sub = rospy.Subscriber("camera/rgb/image_color", Image, self.callback)
+		self.sub = rospy.Subscriber("camera/rgb/image_color/compressed", Image, self.callback)
 		self.pub = rospy.Publisher("decompressed", Image)
 
 	def callback(self, data):
 		# figure out python version of
 		# rosrun image_transport republish in:=/camera/rgb/image_color compressed out:=/camera/rgb/image_color raw
+		# http://wiki.ros.org/image_transport
+		# "image_transport does not yet support Python, though it is on the Roadmap" :(
 		self.pub.publish(data)
 
 
@@ -28,7 +30,7 @@ class Converter(object):
 		except CvBridgeError, e:
 			rospy.loginfo(e)
 
-		# do stuff to self.cv_image
+		# manipulate self.cv_image, send to convnet for prediction
 		rospy.loginfo(self.cv_image.shape)
 
 
