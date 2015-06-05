829a616c7a8c71f92fd3fe24205e4b9bb2dc10dc
SeanCCarter <seancarter26@gmail.com>
2015-06-05 17:03:42 -0400

    Testing network for Char74k

---
+++ modern_net/Char74knet.py
@@ -0,0 +1,26 @@
+"This will be used to create a neural net modeling the Char74k data."
+
+import theano
+import numpy as np
+import cPickle
+import mnet_general
+
+if __name__ == '__main__':
+	print "Initilizing network"
+	mnet = mnet_general.ModernNeuralNetwork([784,625,860, 62])
+
+	print "Loading Data"
+	f = open("./Alphabet/Char74k_data.save",'rb')
+	trX = cPickle.load(f)
+	trY = np.array(cPickle.load(f))
+	f.close()
+	print trX.shape
+	print trY.shape
+
+	testing=np.random.randint(10000,size=1000)
+	teX = trX[testing,:]
+	teY = trY[testing]
+	print teX.shape
+	print teY.shape
+
+
