3c80f81c465e6dfff5659d50c2c85647493b3dc7
SeanCCarter <seancarter26@gmail.com>
2015-06-08 13:37:16 -0700

    Bugfixes to Char74k stuff.

--- modern_net/Char74_data_debug.py
+++ modern_net/Char74_data_debug.py
@@ -1,32 +1,26 @@
 import cv2
 import numpy as np
 import sys
+import cPickle
 
 def shuffle_in_unison_scary(a, b):
-    rng_state = numpy.random.get_state()
-    numpy.random.shuffle(a)
-    numpy.random.set_state(rng_state)
-    numpy.random.shuffle(b)
+    rng_state = np.random.get_state()
+    np.random.shuffle(a)
+    np.random.set_state(rng_state)
+    np.random.shuffle(b)
 
 
 if __name__ == '__main__':
 
 	print "Loading Data"
 	f = open("./Alphabet/Char74k_data.save",'rb')
-	trX = cPickle.load(f).reshape(-1,100,100)
-	trY = reprocess(np.array(cPickle.load(f)))
+	trX = cPickle.load(f)
+	trY = cPickle.load(f)
 	f.close()
 
 	shuffle_in_unison_scary(trX, trY)
 	image_index = 0
 
-	while(True):
-		image = trX[image_index, :, :]
+	image = trX[image_index].reshape((100,100))
 
-		cv2.imshow(image, cv2.resize(image, (640,640)))
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-
-	#This closes everything down, so that I don't accidentally break Ubuntu again
-	webcam_video.release()
-	cv2.destroyAllWindows()
+	cv2.imshow("window", image)
--- modern_net/Char74knet.py
+++ modern_net/Char74knet.py
@@ -5,6 +5,8 @@ import numpy as np
 import cPickle
 import mnet_general
 import string
+from theano.sandbox.cuda.basic_ops import host_from_gpu
+
 
 def save_weights(weights, filename):
 		length = reduce(lambda x,y: x*y, weights.shape.eval())
@@ -17,36 +19,43 @@ def save_weights(weights, filename):
 		f.close()
 
 def reprocess(data):
-	newdata = np.zeros(len(data), 62)
-	for i in len(data):
+	newdata = np.zeros((len(data), 62))
+	for i in xrange(len(data)):
 		newdata[i:data[i]] = 1
 	return newdata
 
+def shuffle_in_unison(a, b):
+	rng_state = np.random.get_state()
+	np.random.shuffle(a)
+	np.random.set_state(rng_state)
+	np.random.shuffle(b)
+
 if __name__ == '__main__':
 	print "Initilizing network"
-	mnet = mnet_general.ModernNeuralNetwork([784,625,860, 62])
+	mnet = mnet_general.ModernNeuralNetwork([10000,625,860, 62])
 	mnet.create_model_functions()
 
 	print "Loading Data"
 	f = open("./Alphabet/Char74k_data.save",'rb')
-	trX = cPickle.load(f).reshape(-1,100,100)
+	trX = cPickle.load(f).reshape(-1,100,100).reshape(-1,10000)
 	trY = reprocess(np.array(cPickle.load(f)))
+	shuffle_in_unison(trX, trY)
 	f.close()
 
 	print "Creating Testing Data"
 	testing=np.random.randint(len(trX),size=700)
 	teX = trX[testing,:]
 	teY = trY[testing,:]
+	shuffle_in_unison(teX, teY)
 
 	print "Training Net:"
 	for i in range(10):
 		for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
-			print trX[start:end].shape
 			cost = mnet.train(trX[start:end], trY[start:end])
 		print np.mean(np.argmax(teY, axis=1) == mnet.predict(teX))
+		#print mnet.predict(teX)
 
 	print "Saving Data"
-	for counter, weight in mnet.weights:
-		save_weights(weight, "Weight{1}.save".format(counter))
-
+	for counter, weight in enumerate(mnet.weights):
+		save_weights(weight, "./weights/Weight{0}.save".format(counter))
 
