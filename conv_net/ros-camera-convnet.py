1e7c8ad9713ecc42687abc175c5bd80fcadd5116
joeylmaalouf <joeylmaalouf@gmail.com>
2015-06-08 10:01:13 -0400

    ros integration works fine, but we should figure out compression stuff

--- conv_net/ros-camera-convnet.py
+++ conv_net/ros-camera-convnet.py
@@ -1,12 +1,17 @@
 import Tkinter as tk
-from PIL import Image, ImageTk
+from PIL import Image as PImage
+from PIL import ImageTk
 import numpy as np
+import roslib
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 import cv2
 import convnet
 
 
 class VideoWindow(object):
-	def __init__(self, window_root, video_capture):
+	def __init__(self, window_root):
 		super(VideoWindow, self).__init__()
 
 		self.cnn = convnet.ConvolutionalNeuralNetwork()
@@ -14,21 +19,22 @@ class VideoWindow(object):
 		self.cnn.create_model_functions()
 
 		self.root = window_root
-		self.cap = video_capture
 		self.root.bind("<Escape>", lambda e: self.root.quit())
-		self.label1 = tk.Label(self.root, image = None)
-		self.label1.pack()
-		self.label2 = tk.Label(self.root, image = None)
-		self.label2.pack()
+		self.label = tk.Label(self.root, image = None)
+		self.label.pack()
 		self.button = tk.Button(self.root, text = "Send image to convnet as 28x28 inverted digit picture.", command = self.predict_class)
 		self.button.pack()
 
-	def get_frame(self):
-		ret, frame = self.cap.read()
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		rospy.init_node("listener", anonymous = True)
+		self.sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.callback)
+		self.bridge = CvBridge()
+		self.frame = np.zeros((480, 480), dtype = np.uint8)
 
-		w = self.cap.get(3)
-		h = self.cap.get(4)
+	def callback(self, data):
+		frame = self.bridge.imgmsg_to_cv2(data, "rgb8")
+
+		w = frame.shape[1]
+		h = frame.shape[0]
 		if w > h:
 			offset = (w-h)/2
 			frame = frame[:, offset:w-offset]
@@ -36,25 +42,14 @@ class VideoWindow(object):
 			offset = (h-w)/2
 			frame = frame[offset:h-offset, :]
 
-		return frame
+		self.frame = frame
 
 	def update_frames(self):
-		frame = self.draw_rect(self.get_frame())
-		imgtk = ImageTk.PhotoImage(image = Image.fromarray(frame))
-		self.label1.configure(image = imgtk)
-		self.label1.image = imgtk
-		frame = self.manipulate(frame)
-		imgtk = ImageTk.PhotoImage(image = Image.fromarray(frame))
-		self.label2.configure(image = imgtk)
-		self.label2.image = imgtk
+		frame = self.draw_rect(self.frame)
+		imgtk = ImageTk.PhotoImage(image = PImage.fromarray(frame))
+		self.label.configure(image = imgtk)
+		self.label.image = imgtk
 		self.root.after(100, self.update_frames)
-
-	def manipulate(self, frame):
-		x1, x2, y1, y2 = self.get_bounds(frame)
-		frame = frame[y1:y2, x1:x2]
-		frame = cv2.resize(frame, (28, 28))
-		cv2.bitwise_not(frame, frame) # MNIST is white on black, our examples are black on white
-		return frame
 
 	def draw_rect(self, frame):
 		img = np.copy(frame)
@@ -71,13 +66,19 @@ class VideoWindow(object):
 		y2 = 3*h/4
 		return x1, x2, y1, y2
 
+	def manipulate(self, frame):
+		x1, x2, y1, y2 = self.get_bounds(frame)
+		frame = frame[y1:y2, x1:x2]
+		frame = cv2.resize(frame, (28, 28))
+		cv2.bitwise_not(frame, frame) # MNIST is white on black, our examples are black on white
+		return frame
+
 	def run(self):
 		self.update_frames()
 		self.root.mainloop()
-		self.cap.release()
 
 	def predict_class(self):
-		frame = self.manipulate(self.get_frame())
+		frame = self.manipulate(self.frame)
 		frame = frame.reshape(-1, 1, 28, 28)
 		prediction = self.cnn.predict(frame)[0]
 		print("Predicted digit: {0}".format(prediction))
@@ -85,5 +86,5 @@ class VideoWindow(object):
 
 
 if __name__ == "__main__":
-	vwin = VideoWindow(tk.Tk(), cv2.VideoCapture(0))
+	vwin = VideoWindow(tk.Tk())
 	vwin.run()
