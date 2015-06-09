9d5e8ea40203c9eb209fe07680bf225981a4d97b
joeylmaalouf <joeylmaalouf@gmail.com>
2015-06-09 16:11:40 -0400

    potential fix for activation conversion to np array: recalculate

--- conv_net/convnet.py
+++ conv_net/convnet.py
@@ -16,7 +16,7 @@ class ConvolutionalNeuralNetwork(object):
 		self.Y = T.fmatrix()
 
 	def floatX(self, X):
-		return np.asarray(X, dtype=theano.config.floatX)
+		return np.asarray(X, dtype = theano.config.floatX)
 
 	def init_weights(self, shape):
 		return theano.shared(self.floatX(np.random.randn(*shape) * 0.01))
@@ -25,18 +25,18 @@ class ConvolutionalNeuralNetwork(object):
 		return T.maximum(X, 0.)
 
 	def softmax(self, X):
-		e_x = T.exp(X - X.max(axis=1).dimshuffle(0, "x"))
-		return e_x / e_x.sum(axis=1).dimshuffle(0, "x")
+		e_x = T.exp(X - X.max(axis = 1).dimshuffle(0, "x"))
+		return e_x / e_x.sum(axis = 1).dimshuffle(0, "x")
 
-	def dropout(self, X, p=0.):
+	def dropout(self, X, p = 0.):
 		if p > 0:
 			retain_prob = 1 - p
-			X *= self.srng.binomial(X.shape, p=retain_prob, dtype=theano.config.floatX)
+			X *= self.srng.binomial(X.shape, p = retain_prob, dtype = theano.config.floatX)
 			X /= retain_prob
 		return X
 
-	def RMSprop(self, cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
-		grads = T.grad(cost=cost, wrt=params)
+	def RMSprop(self, cost, params, lr = 0.001, rho = 0.9, epsilon = 1e-6):
+		grads = T.grad(cost = cost, wrt = params)
 		updates = []
 		for p, g in zip(params, grads):
 			acc = theano.shared(p.get_value() * 0.)
@@ -48,7 +48,7 @@ class ConvolutionalNeuralNetwork(object):
 		return updates
 
 	def model(self, X, w1, w2, w3, w4, wo, p_drop_conv, p_drop_hidden):
-		l1a = self.rectify(conv2d(X, w1, border_mode='full'))
+		l1a = self.rectify(conv2d(X, w1, border_mode = "full"))
 		l1 = max_pool_2d(l1a, (2, 2))
 		l1 = self.dropout(l1, p_drop_conv)
 
@@ -58,7 +58,7 @@ class ConvolutionalNeuralNetwork(object):
 
 		l3a = self.rectify(conv2d(l2, w3))
 		l3b = max_pool_2d(l3a, (2, 2))
-		l3 = T.flatten(l3b, outdim=2)
+		l3 = T.flatten(l3b, outdim = 2)
 		l3 = self.dropout(l3, p_drop_conv)
 
 		l4 = self.rectify(T.dot(l3, w4))
@@ -68,7 +68,7 @@ class ConvolutionalNeuralNetwork(object):
 		return l1, l2, l3, l4, pyx
 
 	def initialize_mnist(self):
-		self.trX, self.teX, self.trY, self.teY = mnist(onehot=True)
+		self.trX, self.teX, self.trY, self.teY = mnist(onehot = True)
 
 		self.trX = self.trX.reshape(-1, 1, 28, 28)
 		self.teX = self.teX.reshape(-1, 1, 28, 28)
@@ -82,21 +82,22 @@ class ConvolutionalNeuralNetwork(object):
 	def create_model_functions(self):
 		self.noise_l1, self.noise_l2, self.noise_l3, self.noise_l4, self.noise_py_x = self.model(self.X, self.w1, self.w2, self.w3, self.w4, self.wo, 0.2, 0.5)
 		self.l1, self.l2, self.l3, self.l4, self.py_x = self.model(self.X, self.w1, self.w2, self.w3, self.w4, self.wo, 0., 0.)
-		self.y_x = T.argmax(self.py_x, axis=1)
+		self.y_x = T.argmax(self.py_x, axis = 1)
 
 		self.cost = T.mean(T.nnet.categorical_crossentropy(self.noise_py_x, self.Y))
 		self.params = [self.w1, self.w2, self.w3, self.w4, self.wo]
 		self.updates = self.RMSprop(self.cost, self.params, lr=0.001)
 
-		self.train = theano.function(inputs=[self.X, self.Y], outputs=self.cost, updates=self.updates, allow_input_downcast=True)
-		self.predict = theano.function(inputs=[self.X], outputs=self.y_x, allow_input_downcast=True)
+		self.train = theano.function(inputs = [self.X, self.Y], outputs = self.cost, updates = self.updates, allow_input_downcast = True)
+		self.predict = theano.function(inputs = [self.X], outputs = self.y_x, allow_input_downcast = True)
+		self.activate = theano.function(inputs = [self.X], outputs = self.l4, allow_input_downcast = True)
 
 	def train_mnist(self, verbose, epochs = 10):
 		for i in range(epochs):
 			for start, end in zip(range(0, len(self.trX), 128), range(128, len(self.trX), 128)):
 				self.cost = self.train(self.trX[start:end], self.trY[start:end])
 			if verbose:
-				print np.mean(np.argmax(self.teY, axis=1) == self.predict(self.teX))
+				print(np.mean(np.argmax(self.teY, axis = 1) == self.predict(self.teX)))
 
 	def save_data(self, filename, data):
 		length = reduce(lambda x,y: x*y, data.shape.eval())
@@ -129,20 +130,6 @@ class ConvolutionalNeuralNetwork(object):
 		self.w4 = self.load_data("weights/w4.txt", (128 * 3 * 3, 625))
 		self.wo = self.load_data("weights/wo.txt", (625, 10))
 
-	def save_all_activations(self):
-		self.save_data("activations/l1.txt", self.l1)
-		self.save_data("activations/l2.txt", self.l2)
-		self.save_data("activations/l3.txt", self.l3)
-		self.save_data("activations/l4.txt", self.l4)
-		self.save_data("activations/py_x.txt", self.py_x)
-
-	def load_all_activations(self):
-		self.l1 = self.load_data("activations/l1.txt", (32, 1, 3, 3))
-		self.l2 = self.load_data("activations/l2.txt", (64, 32, 3, 3))
-		self.l3 = self.load_data("activations/l3.txt", (128, 64, 3, 3))
-		self.l4 = self.load_data("activations/l4.txt", (128 * 3 * 3, 625))
-		self.py_x = self.load_data("activations/py_x.txt", (625, 10))
-
 	def mnist_example(self, verbose = False, save = False):
 		self.initialize_mnist()
 		self.create_model_functions()
@@ -150,8 +137,9 @@ class ConvolutionalNeuralNetwork(object):
 		if save:
 			self.save_all_weights()
 			print("Saved weights to \"./weights/*.txt\".")
-			self.save_all_activations()
-			print("Saved activations to \"./activations/*.txt\".")
+			self.save_data("activations/l4.txt", self.activate(self.teX))
+			print(self.l4.shape)
+			print("Saved penultimate activations to \"./activations/l4.txt\".")
 
 
 if __name__ == "__main__":
