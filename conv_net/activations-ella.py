6b834888fb6b5006998e903c4061ee628ddf4937
joeylmaalouf <joeylmaalouf@gmail.com>
2015-06-11 13:48:07 -0400

    proof of concept works! logistic regression does well with activations from neural network, accuracy increased from 99.3% to 99.4%

--- conv_net/activations-ella.py
+++ conv_net/activations-ella.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 
 
 if __name__ == "__main__":
-	print("\nLoading Data:")
+	print("\nLoading Data...")
 	load_activations = convnet.ConvolutionalNeuralNetwork().load_data
 	num_chunks = 20
 	trAs = [load_activations("saved/trA{0:02d}.txt".format(i), (60000/num_chunks, 625)) for i in range(num_chunks)]
@@ -15,23 +15,29 @@ if __name__ == "__main__":
 	teA = load_activations("saved/teA.txt", (10000, 625))
 	print("teA.shape: {0}".format(teA.shape))
 	trX, teX, trY, teY = load.mnist(onehot = True)
+	trC = np.argmax(trY, axis = 1)
+	print("trC.shape: {0}".format(trC.shape))
+	teC = np.argmax(teY, axis = 1)
+	print("teC.shape: {0}".format(teC.shape))
+	print("Done.")
 
-	print("\nCreating Regression Model:")
-	classes = np.argmax(trY, axis = 1)
-	print("classes.shape: {0}".format(classes.shape))
+	print("\nCreating Regression Model...")
 	lr = LogisticRegression()
-	lr.fit(trA, classes)
+	lr.fit(trA, trC)
+	print("Done.")
 
-	print("\nAnalyzing Training Data:")
+	print("\nAnalyzing Training Data...")
 	predictions = lr.predict(trA)
 	print("predictions.shape: {0}".format(predictions.shape))
-	accuracy = np.mean(predictions == classes)
+	accuracy = np.mean(predictions == trC)
 	print("accuracy: {0}".format(accuracy))
+	print("Done.")
 
-	print("\nAnalyzing Testing Data:")
+	print("\nAnalyzing Testing Data...")
 	predictions = lr.predict(teA)
 	print("predictions.shape: {0}".format(predictions.shape))
-	accuracy = np.mean(predictions == classes)
+	accuracy = np.mean(predictions == teC)
 	print("accuracy: {0}".format(accuracy))
+	print("Done.")
 
-	print("\n")
+	print("\nExecution complete. Program terminated.\n")
