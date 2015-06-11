c384f3b3ed1bbc3175499a50355c3b3c7a1f5575
joeylmaalouf <joeylmaalouf@gmail.com>
2015-06-11 13:37:01 -0400

    detailed logging

--- conv_net/activations-ella.py
+++ conv_net/activations-ella.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 
 
 if __name__ == "__main__":
+	print("\nLoading Data:")
 	load_activations = convnet.ConvolutionalNeuralNetwork().load_data
 	num_chunks = 20
 	trAs = [load_activations("saved/trA{0:02d}.txt".format(i), (60000/num_chunks, 625)) for i in range(num_chunks)]
@@ -14,11 +15,23 @@ if __name__ == "__main__":
 	teA = load_activations("saved/teA.txt", (10000, 625))
 	print("teA.shape: {0}".format(teA.shape))
 	trX, teX, trY, teY = load.mnist(onehot = True)
+
+	print("\nCreating Regression Model:")
 	classes = np.argmax(trY, axis = 1)
 	print("classes.shape: {0}".format(classes.shape))
 	lr = LogisticRegression()
 	lr.fit(trA, classes)
+
+	print("\nAnalyzing Training Data:")
+	predictions = lr.predict(trA)
+	print("predictions.shape: {0}".format(predictions.shape))
+	accuracy = np.mean(predictions == classes)
+	print("accuracy: {0}".format(accuracy))
+
+	print("\nAnalyzing Testing Data:")
 	predictions = lr.predict(teA)
 	print("predictions.shape: {0}".format(predictions.shape))
 	accuracy = np.mean(predictions == classes)
 	print("accuracy: {0}".format(accuracy))
+
+	print("\n")
