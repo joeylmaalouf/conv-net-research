a9a1161d8b82e6f02b9358d93bd53bdffc1a6e8d
joeylmaalouf <joeylmaalouf@gmail.com>
2015-06-12 15:15:20 -0400

    ELLA integration

--- conv_net/activations-ella.py
+++ conv_net/activations-ella.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 	print("\nLoading Data...")
 	load_activations = convnet.ConvolutionalNeuralNetwork().load_data
 	num_chunks = 20
-	trAs = [load_activations("saved/trA{0:02d}.txt".format(i), (60000/num_chunks, 625)) for i in range(num_chunks)]
+	trAs = [load_activations("saved/trA{0:02d}.txt".format(i), (60000 / num_chunks, 625)) for i in range(num_chunks)]
 	trA = np.concatenate(trAs)
 	print("trA.shape: {0}".format(trA.shape))
 	teA = load_activations("saved/teA.txt", (10000, 625))
@@ -27,20 +27,33 @@ if __name__ == "__main__":
 
 	print("\nCreating ELLA Model...")
 	num_params = 625
-	num_latent = 5
-	ella = ELLA.ELLA(num_params, num_latent, LogisticRegression)
+	num_latent = 20
+	ella = ELLA.ELLA(num_params, num_latent, LogisticRegression, mu = 10 ** -3)
 	for task in range(10):
 		result_vector = binarize(trC, task)
 		ella.fit(trA, result_vector, task)
 		print("Trained task {0}".format(task))
+	print("Sparsity coefficients: {0}".format(ella.S))
 	print("Done.")
 
 	print("\nAnalyzing Training Data...")
-	pass
+	predictions = np.argmax(np.asarray([ella.predict_logprobs(trA, i) for i in range(ella.T)]), axis = 0)
+	print("predictions.shape: {0}".format(predictions.shape))
+	accuracy = np.mean(predictions == trC)
+	print("accuracy: {0:0.04f}".format(accuracy))
+	print("per-task binary accuracy:")
+	for task_id in range(ella.T):
+		print("  {0} - {1:0.04f}".format(task_id, np.mean(binarize(predictions, task_id) == binarize(trC, task_id))))
 	print("Done.")
 
 	print("\nAnalyzing Testing Data...")
-	pass
+	predictions = np.argmax(np.asarray([ella.predict_logprobs(teA, i) for i in range(ella.T)]), axis = 0)
+	print("predictions.shape: {0}".format(predictions.shape))
+	accuracy = np.mean(predictions == teC)
+	print("accuracy: {0:0.04f}".format(accuracy))
+	print("per-task binary accuracy:")
+	for task_id in range(ella.T):
+		print("  {0} - {1:0.04f}".format(task_id, np.mean(binarize(predictions, task_id) == binarize(teC, task_id))))
 	print("Done.")
 
 	print("\nExecution complete.\nProgram terminated successfully.\n")
