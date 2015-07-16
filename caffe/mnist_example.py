51115dc24c4ffd2a47ac21aabf20b8fd8b5d3a16
joeylmaalouf <joeylmaalouf@gmail.com>
2015-07-16 09:38:08 -0400

    made things into functions

--- caffe/mnist_example.py
+++ caffe/mnist_example.py
@@ -16,8 +16,9 @@ from caffe import layers as L
 from caffe import params as P
 
 
-# create the net and save its structure
-def lenet(lmdb, batch_size):
+def lenet(lmdb, batch_size = 200):
+	""" Create our net structure.
+	"""
 	n = caffe.NetSpec()
 	n.data, n.label = L.Data(batch_size = batch_size, backend = P.Data.LMDB, source = lmdb, transform_param = dict(scale = 1./255), ntop = 2)
 	n.conv1 = L.Convolution(n.data, kernel_size = 5, num_output = 20, weight_filler = dict(type = "xavier"))
@@ -30,53 +31,69 @@ def lenet(lmdb, batch_size):
 	n.loss = L.SoftmaxWithLoss(n.ip2, n.label)
 	return n.to_proto()
 
-with open("examples/mnist/lenet_auto_train.prototxt", "w") as f:
-	f.write(str(lenet("examples/mnist/mnist_train_lmdb", 200)))
 
-with open("examples/mnist/lenet_auto_test.prototxt", "w") as f:
-	f.write(str(lenet("examples/mnist/mnist_test_lmdb", 200)))
+def make_prototxts(batch_size = 200):
+	""" Save the net structure to prototxt files.
+	"""
+	with open("examples/mnist/lenet_auto_train.prototxt", "w") as f:
+		f.write(str(lenet("examples/mnist/mnist_train_lmdb", batch_size)))
+	with open("examples/mnist/lenet_auto_test.prototxt", "w") as f:
+		f.write(str(lenet("examples/mnist/mnist_test_lmdb", batch_size)))
 
 
-# load the net structure
-caffe.set_device(0)
-caffe.set_mode_gpu()
-solver = caffe.SGDSolver("examples/mnist/lenet_auto_solver.prototxt")
+def load_solver(verbose = True):
+	""" Create a solver from the loaded net structure.
+	"""
+	caffe.set_device(0)
+	caffe.set_mode_gpu()
+	solver = caffe.SGDSolver("examples/mnist/lenet_auto_solver.prototxt")
 
-print("Model structure:")
-for k, v in solver.net.blobs.items():
-	print("  {0} of shape {1}".format(k, v.data.shape))
+	if verbose:
+		print("Model structure:")
+		for k, v in solver.net.blobs.items():
+			print("  {0} of shape {1}".format(k, v.data.shape))
 
-# initialize the nets
-solver.net.forward()
-solver.test_nets[0].forward()
+	return solver
 
-epochs = 200
-for _ in range(epochs):
-	# step forward in the training, starting from the conv layer because starting at the input layer would reload the data
-	solver.step(1)
-	solver.net.forward(start = "conv1")
-	solver.test_nets[0].forward(start = "conv1")
 
-# predictions are the argmax"d values from the final layer, "ip2"
-# training accuracy
-# print np.mean(solver.net.blobs["ip2"].data.argmax(1) == solver.net.blobs["label"].data)
-# testing accuracy
-print("Accuracy: {0}".format(np.mean(solver.test_nets[0].blobs["ip2"].data.argmax(1) == solver.test_nets[0].blobs["label"].data)))
+def run_solver(solver, epochs = 100):
+	""" Run the given solver and return its testing
+		accuracy after a certain number of epochs.
+	"""
+	# initialize the nets
+	solver.net.forward()
+	solver.test_nets[0].forward()
 
-# experimenting with db stuff
-# env = lmdb.Environment("examples/mnist/mnist_train_lmdb", map_size = 100000000)
-# db = env.open_db()
-# txn = lmdb.Transaction(env, db)
-# csr = lmdb.Cursor(db, txn)
-train_cursor = lmdb.open("examples/mnist/mnist_train_lmdb", map_size = 100000000).begin(write = True).cursor()
-test_cursor = lmdb.open("examples/mnist/mnist_test_lmdb", map_size = 100000000).begin(write = True).cursor()
+	for _ in range(epochs):
+		# step forward in the training, starting from the conv layer because starting at the input layer would reload the data
+		solver.step(1)
+		solver.net.forward(start = "conv1")
+		solver.test_nets[0].forward(start = "conv1")
 
-#for key, value in train_cursor:
-#	print key, value
-#for key, value in test_cursor:
-#	print key, value
-print("{0} training samples".format(sum(1 for _ in train_cursor)))
-print("{0} testing samples".format(sum(1 for _ in test_cursor)))
+	# predictions are the argmax"d values from the final layer, "ip2"
+	# training accuracy
+	# print np.mean(solver.net.blobs["ip2"].data.argmax(1) == solver.net.blobs["label"].data)
+	# testing accuracy
+	return np.mean(solver.test_nets[0].blobs["ip2"].data.argmax(1) == solver.test_nets[0].blobs["label"].data)
+
+
+def load_db_cursors(train_db_path, test_db_path, verbose = True):
+	""" Load the given databases and return cursors created from them.
+	"""
+	train_cursor = lmdb.open(train_db_path, map_size = 100000000).begin(write = True).cursor()
+	test_cursor = lmdb.open(test_db_path, map_size = 100000000).begin(write = True).cursor()
+
+	# for key, value in train_cursor:
+		# print key, value
+	# for key, value in test_cursor:
+		# print key, value
+
+	if verbose:
+		print("{0} training samples".format(sum(1 for _ in train_cursor)))
+		print("{0} testing samples".format(sum(1 for _ in test_cursor)))
+
+	return train_cursor, test_cursor
+
 
 def rewrite_binarized(db_cursor, datum_id, task):
 	string = db_cursor.get(datum_id)
@@ -85,11 +102,19 @@ def rewrite_binarized(db_cursor, datum_id, task):
 	string = datum.SerializeToString()
 	db_cursor.put(datum_id, string, overwrite = True)
 
+
 def data_from_db(db_cursor, datum_id):
 	string = db_cursor.get(datum_id)
 	datum = caffe.proto.caffe_pb2.Datum.FromString(string)	
 	return caffe.io.datum_to_array(datum), datum.label
 
-print data_from_db(train_cursor, "00000000")[1]
-rewrite_binarized(train_cursor, "00000000", 0)
-print data_from_db(train_cursor, "00000000")[1]
+
+if __name__ == "__main__":
+	make_prototxts(batch_size = 200)
+	solver = load_solver(verbose = True)
+	accuracy = run_solver(solver, epochs = 200)
+	print("Accuracy: {0}".format(accuracy))
+	train_cursor, test_cursor = load_db_cursors("examples/mnist/mnist_train_lmdb", "examples/mnist/mnist_test_lmdb", True)
+	print data_from_db(train_cursor, "00000000")[1]
+	rewrite_binarized(train_cursor, "00000000", 0)
+	print data_from_db(train_cursor, "00000000")[1]
