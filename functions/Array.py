3b24a2e096ba8d831acdffc238abde01c83e6c08
joeylmaalouf <joeylmaalouf@gmail.com>
2015-07-20 14:35:40 -0400

    array crop sampling function

--- functions/Array.py
+++ functions/Array.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 
 
@@ -9,8 +10,23 @@ def binarize(data, val, dtype = np.uint8):
 	return (np.asarray(data) == val).astype(dtype)
 
 
+def crop_sampling(original, cropped_size):
+	""" Given a 2D NumPy array and a cropped size <= the size of the
+		original array, return a list of all possible cropped variations.
+	"""
+	corners = itertools.product(range(0, 1+original.shape[0]-cropped_size[0]), range(0, 1+original.shape[1]-cropped_size[1]))
+	return [original[c[0]:(c[0]+cropped_size[0]), c[1]:(c[1]+cropped_size[1])] for c in corners]
+
+
 if __name__ == "__main__":
 	l = [0, 6, 1, 3, 4, 1, 5, 2, 5, 3]
 	v = 3
 	print("{0} == {1}?".format(np.asarray(l), v))
 	print(binarize(l, v))
+
+	image = np.reshape(np.arange(16), (4, 4))
+	print("\nOriginal array:")
+	print(image)
+	print "Cropped sub-arrays:"
+	for c in crop_sampling(image, (3, 2)):
+		print(c)
