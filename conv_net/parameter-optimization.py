b4fb386507f88962a12d5ba20b907334928a735e
joeylmaalouf <joeylmaalouf@gmail.com>
2015-07-27 14:02:15 -0700

    wrapper absorbed more of net object in order to train more parameters (because I didn't just add arguments to the net functions instead lol)

--- conv_net/parameter-optimization.py
+++ conv_net/parameter-optimization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+from theano import tensor as T
 
 import convnet
 sys.path.append("../")
@@ -20,12 +21,25 @@ class ConvWrapper(object):
 		super(ConvWrapper, self).__init__()
 		self.net = convnet.ConvolutionalNeuralNetwork()
 		self.net.initialize_mnist()
-		self.net.create_model_functions()
+
 
 	def prep(self):
 		# all of this could've gone at the beginning of eval(),
 		# but i wanted that to just be the accuracy evaluation,
 		# so we'll train the model here in prep()
+		self.net.noise_l1, self.net.noise_l2, self.net.noise_l3, self.net.noise_l4, self.net.noise_py_x = self.net.model(self.net.X, self.net.w1, self.net.w2, self.net.w3, self.net.w4, self.net.wo, self.dropout_conv_rate, self.drop_hidden_rate)
+		self.net.l1, self.net.l2, self.net.l3, self.net.l4, self.net.py_x = self.net.model(self.net.X, self.net.w1, self.net.w2, self.net.w3, self.net.w4, self.net.wo, 0., 0.)
+		self.net.y_x = T.argmax(self.net.py_x, axis = 1)
+
+		self.net.cost = T.mean(T.nnet.categorical_crossentropy(self.net.noise_py_x, self.net.Y))
+		self.net.params = [self.net.w1, self.net.w2, self.net.w3, self.net.w4, self.net.wo]
+		self.net.updates = self.net.RMSprop(self.net.cost, self.net.params, lr = self.learning_rate)
+
+		self.net.train = theano.function(inputs = [self.net.X, self.net.Y], outputs = self.net.cost, updates = self.net.updates, allow_input_downcast = True)
+		self.net.predict = theano.function(inputs = [self.net.X], outputs = self.net.y_x, allow_input_downcast = True)
+		self.net.predict_probs = theano.function(inputs = [self.net.X], outputs = self.net.py_x, allow_input_downcast = True)
+		self.net.activate = theano.function(inputs = [self.net.X], outputs = self.net.l4, allow_input_downcast = True)
+
 		for i in range(self.epochs):
 			for start, end in zip(range(0, len(self.net.trX), self.batch_size), range(self.batch_size, len(self.net.trX), self.batch_size)):
 				self.net.cost = self.net.train(self.net.trX[start:end], self.net.trY[start:end])
@@ -36,7 +50,10 @@ class ConvWrapper(object):
 
 if __name__ == "__main__":
 	paramdict = {
-		"epochs": [0, 5, 10],
-		"batch_size": [100, 150, 200]
+		"epochs": [5, 10, 20],
+		"batch_size": [100, 200],
+		"dropout_conv_rate": [0.2, 0.3, 0.4],
+		"dropout_hidden_rate": [0.2, 0.3, 0.4, 0.5],
+		"learning_rate": [0.0001, 0.001]
 	}
 	print("Best parameter combination: {0}".format(grid_search(ConvWrapper, paramdict, verbose = True)))
