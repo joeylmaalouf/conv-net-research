6b34e528595af9833f15c42606618bf4703e965d
joeylmaalouf <joeylmaalouf@gmail.com>
2015-08-12 14:49:30 -0400

    more unified net initialization

--- conv_net/office_object_convnet.py
+++ conv_net/office_object_convnet.py
@@ -2,5 +2,5 @@ from convnet import ConvolutionalNeuralNetwork
 
 
 if __name__ == "__main__":
-	cnn = ConvolutionalNeuralNetwork().initialize_office().create_model_functions().train_net(epochs = 10, batch_size = 50, verbose = True)
+	cnn = ConvolutionalNeuralNetwork().initialize_dataset("office").create_model_functions().train_net(epochs = 5, batch_size = 50, verbose = True)
 	# method chaining is cool, guys
